a588f43ad3add300f4f25adb6893c3036a1687b5
momoogles <mo1419091@gmail.com>
2022-10-10 15:16:47 +0900

    deleteも実装した

--- internal/deleteUsers.js
+++ internal/deleteUsers.js
@@ -1,22 +1,33 @@
 const fs = require("fs");
 const { parse } = require("csv-parse");
 const { db } = require("./firebase");
-const { writeBatch, doc } = require("firebase/firestore");
+const { writeBatch, doc, collection, getDocs } = require("firebase/firestore");
 const { exit } = require("process");
 
 // 1. rootにusers.csvを 'uid,group' のformatで置く
-// 2. yarn firebase:create-usersを実行する
+// 2. yarn firebase:delete-usersを実行する
 
 async function main() {
   const batch = writeBatch(db);
 
-  console.log("データを作成します.");
+  console.log("データを読み取り中...");
+  const snapshot = await getDocs(collection(db, "users"));
+  let unlockedUsers = [];
+  snapshot.docs.forEach((doc) => {
+    const { locked } = doc.data();
+    if (!locked) {
+      unlockedUsers.push(doc.id);
+    }
+  });
+  console.log("データの読み取りが完了しました.");
+
+  console.log("データを削除します.");
   fs.createReadStream("./users.csv")
     .pipe(parse({ delimiter: ",", from_line: 2 }))
     .on("data", ([uid, group]) => {
       console.log("データ:", [uid, group]);
-      if (uid && group) {
-        batch.set(doc(db, "users", uid), { group, locked: false });
+      if (unlockedUsers.includes(uid) && group) {
+        batch.delete(doc(db, "users", uid));
       }
     })
     .on("end", async () => {
